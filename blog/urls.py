11558b52bc750869edc44b648f83db9d1c9c4868
SangGyun <sgsecdev@gmail.com>
2017-04-07 02:01:57 +0900

    Adding CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.Post_detail, name='post_detail')
+    url(r'^post/(?P<pk>[0-9]+)/$', views.Post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/edit$', views.Post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>[0-9]+)/delete$', views.Post_delete, name='post_delete'),
+    url(r'^post/new/$', views.Post_new, name='post_new'),
 ]
