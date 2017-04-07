c5a7c1a26341c9ea6984c07f98b1f6ec2391e43b
SangGyun <sgsecdev@gmail.com>
2017-04-07 10:45:30 +0900

    Query Set order_by desc

---
+++ blog/migrations/0004_auto_20170406_1422.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_auto_20170406_0127'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='create_date',
+            field=models.DateTimeField(default=datetime.datetime(2017, 4, 6, 5, 22, 1, 973259, tzinfo=utc)),
+        ),
+    ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 # Create your views here.
 def Post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def Post_detail(request, pk):
